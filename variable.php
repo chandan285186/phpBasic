850e1f8272b50decee6188c5b2e1b8e42f6c2e75
unknown <chandan285186@gmail.com>
2023-05-06 00:28:47 +0600

    sum/sub/mul/div

--- variable.php
+++ variable.php
@@ -1,3 +1,27 @@
-<?php
+<!--<?php
  echo "chandan";
 ?>
+-->
+
+<?php
+  $sum = $_POST['number1'] + $_POST['number2'];
+  $sub =$_POST['number1'] - $_POST['number2'];
+  $mul =$_POST['number1'] * $_POST['number2'];
+  $div =$_POST['number1'] / $_POST['number2'];
+  
+  echo 'Sum is: '.$sum.'<br>'.'Sub is: '.$sub.'<br>'.'Multi:'.$mul.'<br>'.'divis:'.$div.'';
+?>
+
+
+<html lang="en">
+<boby>
+<form action="" method="post">
+<input type="text" name="number1" value=""/>
+<input type="text" name="number2" value=""/><br />
+
+<input type="submit" name="submit" value="submit"/>
+</form>
+</boby>
+
+
+</html>
