013d9aab62c0530f3c450f4e38a6ca4654cdffd9
unknown <chandan285186@gmail.com>
2023-05-06 21:40:28 +0600

    oddeven

---
+++ even_odd.php
@@ -0,0 +1,29 @@
+
+
+
+<?php
+$chandan = $_POST['number1']%2;
+if($chandan==0)
+{
+echo'number 1 is even';
+}
+else
+{
+echo'number 1 is odd';
+}
+
+
+?>
+
+
+
+<html lang="en">
+<boby>
+<form action="" method="post">
+<input type="text" name="number1" value=""/>
+<input type="submit" name="submit" value="submit"/>
+</form>
+</boby>
+
+
+</html>
--- variable.php
+++ variable.php
@@ -3,7 +3,7 @@
 ?>
 -->
 
-<?php
+<!--<?php
   $sum = $_POST['number1'] + $_POST['number2'];
   $sub =$_POST['number1'] - $_POST['number2'];
   $mul =$_POST['number1'] * $_POST['number2'];
@@ -11,6 +11,24 @@
   
   echo 'Sum is: '.$sum.'<br>'.'Sub is: '.$sub.'<br>'.'Multi:'.$mul.'<br>'.'divis:'.$div.'';
 ?>
+-->
+
+
+<?php
+$chandan = $_POST['number1']%2;
+if($chandan==0)
+{
+echo'number 1 is even';
+}
+else
+{
+echo'number 1 is odd';
+}
+
+
+?>
+
+
 
 
 <html lang="en">
