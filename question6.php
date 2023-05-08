2be522e056e15e0fbd2571ff18f29397d5b58ef3
unknown <chandan285186@gmail.com>
2023-05-08 20:02:23 +0600

    question5

---
+++ question6.php
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+<html lang="en">
+<boby>
+<form action="" method="post">
+<input type="text" name="number1" value=""/>
+<input type="text" name="number2" value=""/><br /><br />
+
+<input type="submit" name="submit" value="submit"/>
+</form>
+</boby>
+</html>
+
+
+
+<?php
+ $das = $_POST['number1']+ $_POST['number2'];
+ echo "$das";
+?>
