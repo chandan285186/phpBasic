42c22b7ce1855d21e8003dee145292e3e40b31f8
unknown <chandan285186@gmail.com>
2023-05-11 00:06:39 +0600

    question7

---
+++ question7.php
@@ -0,0 +1,35 @@
+
+
+
+
+<html lang="en">
+<head>
+<boby>
+<form action="" method="post">
+<input type="text" name="number1" id="number1" value=""/>
+
+
+<input type="submit" name="submit" value="submit"/>
+</form>
+
+<?php
+  $ckd =$_POST['number1'];
+  
+  for($i=1; $i<=$ckd; $i++)
+  {
+  	echo 'I love my Practice .<br>';
+  }
+   
+?>
+
+
+</boby>
+</head>
+</html>
+
+
+
+
+
+
+
